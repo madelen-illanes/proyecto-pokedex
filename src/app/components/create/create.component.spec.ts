cc405d2d7c0ae9512fe374c4eecf893d46a8cfed
madelen <madelen2811@gmail.com>
2022-07-29 14:47:11 -0500

    cambio test

--- src/app/components/create/create.component.spec.ts
+++ src/app/components/create/create.component.spec.ts
@@ -17,7 +17,28 @@ describe('CreateComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should return invalid Form', () => {
+
+    // Arrange
+    
+    const mockList = {
+      nombre: 'Charmander',
+      imagen: 'https://www.pngmart.com/files/13/Charmander-PNG-HD.png',
+      ataque: 70,
+      defensa: 70,
+    }
+    const nameForm : any= component.createFrom.get('nombre')
+    const imagenForm: any = component.createFrom.get('imagen')
+    const attackForm: any = component.createFrom.get('ataque')
+    const defensaForm: any = component.createFrom.get('defensa')
+    //Act
+    
+    nameForm.setValue(mockList.nombre)
+    imagenForm.setValue(mockList.imagen)
+    attackForm.setValue(mockList.imagen)
+    defensaForm.setValue(mockList.imagen)
+    //Assert
+    
+        expect(component.createFrom.valid).toEqual(true);
+      });
 });
